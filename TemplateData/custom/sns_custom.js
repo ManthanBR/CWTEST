ae5523d689426abe190eb65c76165c5e729eceb1
ManthanBR <155616252+ManthanBR@users.noreply.github.com>
2025-01-17 14:57:29 +0530

    edit

--- TemplateData/custom/sns_custom.js
+++ TemplateData/custom/sns_custom.js
@@ -1,7 +1,30 @@
-// Example JavaScript injection to change the file name
-window.addEventListener("zappar-sns-ready", function(){
-    var saveButton = document.getElementById('zapparSaveButton');
-    if(saveButton){
-         saveButton.setAttribute('download', 'MyCustomVideoName.mp4');
+window.addEventListener("zappar-sns-ready", function () {
+    console.log("Zappar SNS ready event fired!");
+
+    const targetNode = document.getElementById('ZapparSnapshotContainer');
+
+    if (!targetNode) {
+        console.error("Zappar Snapshot container not found");
+        return;
     }
+
+    const observer = new MutationObserver(function (mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                console.log("child list mutated, checking for download button");
+                const saveButton = document.getElementById('zapparSaveButton');
+                if (saveButton) {
+                    console.log("Save button found!");
+                    // Remove the initial download attribute
+                    saveButton.removeAttribute('download');
+                    // Set the custom download attribute
+                    saveButton.setAttribute('download', 'MyCustomVideoName.mp4');
+                    console.log("Save button download attribute set to MyCustomVideoName.mp4");
+                    observer.disconnect();
+                    return;
+                }
+            }
+        }
+    });
+    observer.observe(targetNode, { childList: true, subtree: true });
 });
